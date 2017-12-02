6496b9a6468e7c0258e8f606071e5fd56a7073b2
Jo Santana <josantana@jogalabs.com>
2017-12-02 14:20:12 -0200

    Fix email validation rule

--- assets/scripts/validate.js
+++ assets/scripts/validate.js
@@ -1,7 +1,7 @@
 
 const name = value => typeof value === 'string' && value.length >= 2;
 
-const email = value => !/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value);
+const email = value => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value);
 
 export {
     name,
