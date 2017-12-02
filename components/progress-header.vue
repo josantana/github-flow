dbdf5473d0661e10c22e024b8e80ad6c8406bb6d
Jo Santana <josantana@jogalabs.com>
2017-12-02 15:03:59 -0200

    Add logo and progress-header component

---
+++ components/progress-header.vue
@@ -0,0 +1,36 @@
+
+<template>
+
+    <div class="ProgressHeader">
+        <icon :glyph="logo.homeday" height="32px" />
+    </div>
+
+</template>
+<script>
+
+    import icon from '~/components/utils/icon';
+    import homedayLogo from '~/static/vectors/logo-homeday.svg';
+
+    export default {
+        name: 'progress-header',
+        components: {
+            icon,
+        },
+        data() {
+            return {
+                logo: {
+                    homeday: homedayLogo,
+                },
+            };
+        },
+    };
+
+</script>
+<style lang="scss">
+
+    @import '~assets/scss/colors';
+    @import '~assets/scss/variables';
+
+    // ...
+
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,10 +2,22 @@
 <template>
 
     <div class="Layout Layout-default">
+        <progress-header />
         <nuxt/>
     </div>
 
 </template>
+<script>
+
+    import ProgressHeader from '~/components/progress-header';
+
+    export default {
+        components: {
+            ProgressHeader,
+        },
+    };
+
+</script>
 <style lang="scss">
 
     @import '~assets/scss/base';
