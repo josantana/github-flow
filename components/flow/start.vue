f186e46ea5553f2a1262a2e425f21ca49ed22218
Jo Santana <josantana@jogalabs.com>
2017-12-02 10:39:27 -0200

    Add inital flow step

---
+++ components/flow/start.vue
@@ -0,0 +1,54 @@
+
+<template>
+
+    <div class="FlowStart">
+        <smart-input type="text"
+            v-model="firstName"
+            label="First Name"
+            @input="clearErrorMessagesOnInput"
+        />
+        <smart-input type="text"
+            v-model="lastName"
+            label="Last Name"
+            @input="clearErrorMessagesOnInput"
+        />
+        <smart-input type="text"
+            v-model="githubUsername"
+            label="Github Username"
+            @input="clearErrorMessagesOnInput"
+        />
+    </div>
+
+</template>
+<script>
+
+    import SmartInput from '~/components/smart-input.vue';
+
+    export default {
+        name: 'flow-start',
+        components: {
+            SmartInput,
+        },
+        data() {
+            return {
+                error: null,
+                firstName: null,
+                lastName: null,
+                githubUsername: null,
+            };
+        },
+        methods: {
+            clearErrorMessagesOnInput() {
+                this.error = null;
+            },
+        },
+    };
+
+</script>
+<style scoped lang="scss">
+
+    @import '~assets/scss/variables';
+    
+    // ...
+
+</style>
