4e6061426d4b64ea30910edeb7808d419c3ea291
Jo Santana <josantana@jogalabs.com>
2017-12-03 00:01:06 -0200

    Add header style

--- components/progress-header.vue
+++ components/progress-header.vue
@@ -31,6 +31,31 @@
     @import '~assets/scss/colors';
     @import '~assets/scss/variables';
 
-    // ...
+    .ProgressHeader {
+        background: color($white);
+        box-shadow: 0 1px 4px rgba(color($black), .1);
+        margin-bottom: 36px;
+        padding: 12px 32px 16px;
+        position: relative;
+
+        &::before,
+        &::after {
+            border-radius: 100px / 10px;
+            box-shadow: 0 0 20px rgba(color($black), .25);
+            bottom: 0;
+            content: '';
+            left: 10px;
+            position: absolute;
+            right: 10px;
+            top: 50%;
+            z-index: -1;
+        }
+
+        &::after {
+            left: auto;
+            right: 10px;
+            transform: skew(8deg) rotate(3deg);
+        }
+    }
 
 </style>
